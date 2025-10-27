86b69b0bb2fdeffdb73eeb96568d9585c3322615
Deepak Lakhara <deepaklakhara151@gmail.com>
2025-10-27 18:00:59 +0530

    yeah

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -96,7 +96,7 @@ function Navbar() {
         )}
       </header>
       <div className="h-[10rem] flex items-center justify-center w-full ">
-        <a href="/" className="transition-all text-gray-800 duration-400 text-[2rem] ease-in-out hover:opacity-80 z-10 flex items-center justify-center">
+        <a href="/" className="transition-all text-gray-800 duration-400 text-[3.5rem] ease-in-out hover:opacity-80 z-10 flex items-center justify-center">
           MENU
         </a>
       </div>
